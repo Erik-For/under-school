0bec46136eb17e6ca3b5af5f511dbc822c5d82ec
RWS56 <ruben.steen@skola.taby.se>
2025-01-23 13:50:25 +0100

    commit

--- scene_scripts/snow3.ts
+++ scene_scripts/snow3.ts
@@ -1,17 +1,64 @@
+const kim = {
+    bigsprite: new BigSprite (
+        new Sprite("assets/faces.png", 0, 0, 0),
+        new Sprite("assets/faces.png", 1, 0, 0),
+        new Sprite("assets/faces.png", 0, 1, 0),
+        new Sprite("assets/faces.png", 1, 1, 0),
+    )
+}
+
+
 import { BigSprite, NPCTextAnimation } from "../animate.js";
 import { Game, Particle, Snow, Pos } from "../game.js";
 import { Player } from "../player.js";
-import { ObjectBehaviour, Scene, SceneScript, ScriptedObject, TileCoordinate } from "../scene.js";
+import { CollisionRule, ObjectBehaviour, Scene, SceneScript, ScriptedObject, TileCoordinate } from "../scene.js";
+import { Camera } from "../screen.js";
 import { CodeSequenceItem, Sequence, SequenceItem } from "../sequence.js";
 import { Sprite } from "../sprite.js";
 
 export default class Script implements SceneScript {
     name: string = "snow3.js";
+    showKim: boolean = true;
 
     onEnter(prevScene: Scene, game: Game, currentScene: Scene){
-        currentScene.addManyScriptedObjects(
-            //man ska kunna prata med kim på berget nere, samma pos som i snow2 fast nu npc med dialog
-        );
+        if(this.showKim){
+            currentScene.addManyScriptedObjects(
+                //man ska kunna prata med kim på berget nere, samma pos som i snow2 fast nu npc med dialog
+                new ScriptedObject(new Pos(35, -12).multiply(16), ObjectBehaviour.Interactable, "kim", new Sprite("assets/people.png", 4, 0, 0)),
+                new ScriptedObject(new Pos(35, -11).multiply(16), ObjectBehaviour.Interactable, "kim", new Sprite("assets/people.png", 4, 1, 0)),
+                new ScriptedObject(new Pos(33, -31).multiply(16), ObjectBehaviour.ChangeScene, "assets/snow2.json", new Sprite("assets/dungeon.png", 0, 0, 0)),
+                new ScriptedObject(new Pos(34, -31).multiply(16), ObjectBehaviour.ChangeScene, "assets/snow2.json", new Sprite("assets/dungeon.png", 0, 0, 0)),
+            );
+
+            currentScene.registerBehaviour("kim", (game: Game, currentScene: Scene, pos: Pos, data: string) => {
+                            let sequence = new Sequence([
+                                new SequenceItem(new CodeSequenceItem(() => {
+                                    game.getPlayer().freezeMovment();
+                                    game.getInputHandler().preventInteraction();
+                                }), (item, ctx) => { (item as CodeSequenceItem).run(); }),
+                                new SequenceItem(new NPCTextAnimation(kim.bigsprite, "FONClBrISCH, FONClBrISCH, på dig!", 2500, game.getInputHandler()), (item, ctx) => { (item as NPCTextAnimation).render(ctx, game); }),
+                                new SequenceItem(new NPCTextAnimation(kim.bigsprite, "Jag heter Kim, jag var kemilärare på Åva innan de gjorde sig av med mig........ Nu håller jag hus här...", 5000, game.getInputHandler()), (item, ctx) => { (item as NPCTextAnimation).render(ctx, game); }),
+                                new SequenceItem(new NPCTextAnimation(kim.bigsprite, "Du ser inte ut som om du hör hemma här.... Under skolan alltså....", 5000, game.getInputHandler()), (item, ctx) => { (item as NPCTextAnimation).render(ctx, game); }),
+                                new SequenceItem(new NPCTextAnimation(kim.bigsprite, "Jag nej, jag är ett med naturen nu mera...", 2500, game.getInputHandler()), (item, ctx) => { (item as NPCTextAnimation).render(ctx, game); }),
+                                new SequenceItem(new NPCTextAnimation(kim.bigsprite, "Vet du vad, du som kommit så långt på väg till att bli den du vill... Gå tillbaka norr ut, så ska jag hjälpa dig på traven....", 5000, game.getInputHandler()), (item, ctx) => { (item as NPCTextAnimation).render(ctx, game); }),
+                                new SequenceItem(new CodeSequenceItem(() => {
+                                    game.getPlayer().unfreezeMovment();
+                                    game.getInputHandler().allowInteraction();
+                                    //sätt stegar
+                                    currentScene.getTile(new TileCoordinate(41, -31))?.setCollisonRule(CollisionRule.None);
+                                    for(let i = 0; i < 37; i++){
+                                        currentScene.getTile(new TileCoordinate(41, - 32 - i))?.getSprites().pop();
+                                        currentScene.getTile(new TileCoordinate(41, - 32 - i))?.getSprites().push(new Sprite("assets/snowset.png", 0, 3, 0));      
+                                        currentScene.getTile(new TileCoordinate(41, - 32 - i))?.getSprites().push(new Sprite("assets/snowset.png", 6, 4, 1));
+                                  
+                                    }
+                                    game.getCamera().cameraShake(500, 2);
+                                }), (item, ctx) => { (item as CodeSequenceItem).run(); }),
+                            ]);
+            
+                            game.getSequenceExecutor().setSequence(sequence);
+                        });
+        }
 
         game.getPlayer().setPos(new Pos(34, -30).multiply(16));
     };
